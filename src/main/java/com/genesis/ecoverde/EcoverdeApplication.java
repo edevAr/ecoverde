1318e6cfa7cce0be7fda1706f23ac7c8133caa56
edenarielguzmanandia <edenarielguzmanandia@gmail.com>
2025-07-06 17:18:04 -0400

    First commit

---
+++ src/main/java/com/genesis/ecoverde/EcoverdeApplication.java
@@ -0,0 +1,13 @@
+package com.genesis.ecoverde;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcoverdeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcoverdeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/genesis/ecoverde/EcoverdeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.genesis.ecoverde;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcoverdeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
